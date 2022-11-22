e38656b7447b71a0a12acf72de2e88faa9463c5f
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-11-22 13:11:50 -0500

    Fix configuration for HDF5 build.

--- 0_env.sh
+++ 0_env.sh
@@ -1,9 +1,11 @@
 module load PrgEnv-amd
 module load cray-pmi
-module load rocm/5.3.0
+module load amd/5.3.0 # this is how you configure the ROCm version in the Cray programming environment
+module load rocm/5.3.0 # for good measure
 
 if [[ -d spack ]]; then
     . spack/share/spack/setup-env.sh
+    spack env activate ristra-rocmcc
 fi
 
 # Do not look in either $HOME/.spack or any system files. This is to avoid
--- 2_spack.sh
+++ 2_spack.sh
@@ -7,11 +7,18 @@ git clone -b releases/v0.18 -c feature.manyFiles=true https://github.com/spack/s
 . spack/share/spack/setup-env.sh
 
 spack env create ristra-rocmcc
-#spacktivate ristra-rocmcc
 spack env activate ristra-rocmcc
 spack repo add ./flecsi/spack-repo
-spack compiler find
+
+# Find system compilers
+#spack compiler find
+cat compilers.yaml >> spack/var/spack/environments/ristra-rocmcc/spack.yaml
+spack config get compilers
+
+# Find system packages
 cp packages.yaml spack/etc/spack/packages.yaml
 spack external find perl
 spack external find cmake
+
+# Install dependencies
 spack install --only dependencies flecsi@2+graphviz+hdf5 backend=mpi ^cray-mpich@8.1.17%rocmcc@5.3.0

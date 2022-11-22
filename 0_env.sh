bdfb66654b2609569e329ba7a8741125eedd7c68
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-11-21 19:09:31 -0500

    Configuration to keep Spack entirely local. Configure MPI.

--- 0_env.sh
+++ 0_env.sh
@@ -5,3 +5,9 @@ module load rocm/5.3.0
 if [[ -d spack ]]; then
     . spack/share/spack/setup-env.sh
 fi
+
+# Do not look in either $HOME/.spack or any system files. This is to avoid
+# issues where the user's preexiting configuration interferes with this install.
+export SPACK_DISABLE_LOCAL_CONFIG=1
+# After this:
+# packages.yaml will live in spack/etc/spack/packages.yaml
--- 2_spack.sh
+++ 2_spack.sh
@@ -11,6 +11,7 @@ spack env create ristra-rocmcc
 spack env activate ristra-rocmcc
 spack repo add ./flecsi/spack-repo
 spack compiler find
+cp packages.yaml spack/etc/spack/packages.yaml
 spack external find perl
 spack external find cmake
 spack install --only dependencies flecsi@2+graphviz+hdf5 backend=mpi ^mpich@8.1.20%rocmcc@5.3.0

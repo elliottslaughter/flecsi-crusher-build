5def497b955f636583a3dc9d526a18572c65311b
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-11-22 13:18:22 -0500

    GASNet build.

---
+++ 4_gasnet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+cd gasnet
+make -j CONDUIT=ofi-slingshot11 GASNET_INSTALL_DIR=$RISTRA_INSTALL_PREFIX/gasnet CC=cc CXX=CC GASNET_CFLAGS="-fPIC -DGASNETI_FORCE_PIC" GASNET_CXXFLAGS="-fPIC -DGASNETI_FORCE_PIC" LDFLAGS=-fPIC
+cd $RISTRA_INSTALL_PREFIX/gasnet
+make clean
+make -j${THREADS:-16}

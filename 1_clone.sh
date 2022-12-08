ef6e856df5d1c208b4b452b93cf84542c1405d2e
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-12-08 18:14:54 -0500

    Switch to the correct branch of FleCSI.

--- 1_clone.sh
+++ 1_clone.sh
@@ -8,4 +8,4 @@ git clone -b develop --recursive https://github.com/kokkos/kokkos.git
 git clone https://github.com/StanfordLegion/gasnet.git
 git clone -b kokkos-hip-cr --recursive https://gitlab.com/eddy16112/legion.git
 #git clone git@gitlab.lanl.gov:flecsi/flecsi
-git clone https://github.com/flecsi/flecsi.git
+git clone -b 2 https://github.com/flecsi/flecsi.git

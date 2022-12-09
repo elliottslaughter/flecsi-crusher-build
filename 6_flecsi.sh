b9bfc229f2ca0141c405d75d936da02abf61a6d8
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-12-08 19:47:04 -0500

    Build everything release.

--- 6_flecsi.sh
+++ 6_flecsi.sh
@@ -5,7 +5,7 @@ set -e
 source 0_env.sh
 
 cd flecsi
-CC=hipcc CXX=hipcc CFLAGS="-I${MPICH_DIR}/include" CXXFLAGS="-I${MPICH_DIR}/include" LDFLAGS="-L${MPICH_DIR}/lib -lmpi" cmake -DENABLE_FLOG=ON -DENABLE_UNIT_TESTS=ON -DENABLE_HDF5=ON -DENABLE_KOKKOS=on -DCMAKE_INSTALL_PREFIX=$RISTRA_INSTALL_PREFIX/flecsi -Bbuild
+CC=hipcc CXX=hipcc CFLAGS="-I${MPICH_DIR}/include" CXXFLAGS="-I${MPICH_DIR}/include" LDFLAGS="-L${MPICH_DIR}/lib -lmpi" cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_FLOG=ON -DENABLE_UNIT_TESTS=ON -DENABLE_HDF5=ON -DENABLE_KOKKOS=on -DCMAKE_INSTALL_PREFIX=$RISTRA_INSTALL_PREFIX/flecsi -Bbuild
 cd build
 make -j${THREADS:-16}
 #make test # Elliott: tests are still failing
--- 7_poisson.sh
+++ 7_poisson.sh
@@ -5,6 +5,6 @@ set -e
 source 0_env.sh
 
 cd flecsi/tutorial/standalone/poisson
-CC=hipcc CXX=hipcc CFLAGS="-I${MPICH_DIR}/include" CXXFLAGS="-I${MPICH_DIR}/include" LDFLAGS="-L${MPICH_DIR}/lib -lmpi" cmake -Bbuild
+CC=hipcc CXX=hipcc CFLAGS="-I${MPICH_DIR}/include" CXXFLAGS="-I${MPICH_DIR}/include" LDFLAGS="-L${MPICH_DIR}/lib -lmpi" cmake -Bbuild -DCMAKE_BUILD_TYPE=Release
 cd build
 make -j${THREADS:-16}
